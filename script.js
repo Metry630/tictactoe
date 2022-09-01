e7fdcbf93553a22f9d721c24f2b53e35dd8897c6
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2022-09-01 19:55:34 +0700

    Add functions to check current game status

--- script.js
+++ script.js
@@ -1,52 +1,118 @@
 const playerFactory = (name, sign, currentMarkers) => {
-    return { name, sign, currentMarkers }
-}
+  return { name, sign, currentMarkers };
+};
 
-let playerOne = playerFactory("player one", "X", [])
-let playerTwo = playerFactory("player two", "O", [])
+let playerOne = playerFactory("player one", "X", []);
+let playerTwo = playerFactory("player two", "O", []);
 
-let gameBoard = (function(){
-    return {
-
-    }
-})()
+let gameBoard = (function () {
+  return {
+    0: " ",
+    1: " ",
+    2: " ",
+    3: " ",
+    4: " ",
+    5: " ",
+    6: " ",
+    7: " ",
+    8: " ",
+  };
+})();
 
 let displayController = (() => {
-    function checkEnding(currentBoard){
-        console.log('Ending checked')
+  function checkIsFull(currentBoard) {
+    for (i = 0; i <= 9; i++) {
+      if (currentBoard[i] != " ") {
+        return false;
+      }
     }
-    function displayMarkers(gameBoard){
-        for(let i = 0; i <= 8; i++){
-            document.getElementById(i + 1).textContent = gameBoard[i]
-        }
+    return true;
+  }
+  function checkEnding(currentBoard) {
+    console.log(currentBoard);
+    if (
+      currentBoard[0] == currentBoard[1] &&
+      currentBoard[1] == currentBoard[2] &&
+      currentBoard[0] !== " "
+    ) {
+      console.log("game over");
+    } else if (
+      currentBoard[3] == currentBoard[4] &&
+      currentBoard[4] == currentBoard[5] &&
+      currentBoard[3] !== " "
+    ) {
+      console.log("game over");
+    } else if (
+      currentBoard[6] == currentBoard[7] &&
+      currentBoard[7] == currentBoard[8] &&
+      currentBoard[6] !== " "
+    ) {
+      console.log("game over");
+    } else if (
+      currentBoard[0] == currentBoard[4] &&
+      currentBoard[4] == currentBoard[8] &&
+      currentBoard[0] !== " "
+    ) {
+      console.log("game over");
+    } else if (
+      currentBoard[2] == currentBoard[4] &&
+      currentBoard[4] == currentBoard[6] &&
+      currentBoard[2] !== " "
+    ) {
+      console.log("game over");
+    } else if (
+      currentBoard[0] == currentBoard[3] &&
+      currentBoard[3] == currentBoard[6] &&
+      currentBoard[0] !== " "
+    ) {
+      console.log("game over");
+    } else if (
+      currentBoard[1] == currentBoard[4] &&
+      currentBoard[4] == currentBoard[7] &&
+      currentBoard[1] !== " "
+    ) {
+      console.log("game over");
+    } else if (
+      currentBoard[2] == currentBoard[5] &&
+      currentBoard[5] == currentBoard[8] &&
+      currentBoard[2] !== " "
+    ) {
+      console.log("game over");
+    } else if (checkIsFull(currentBoard == true)) {
+      console.log("draw");
     }
-    function placeMarker(gameBoard, boxId){
-        let thisMarker = currentMarker()
-        gameBoard[boxId - 1] = thisMarker;
-        displayMarkers(gameBoard)
+  }
+  function displayMarkers(gameBoard) {
+    for (let i = 0; i <= 8; i++) {
+      document.getElementById(i + 1).textContent = gameBoard[i];
     }
-    let markersUsed = ['O']
-    function currentMarker(){
-        if(markersUsed[markersUsed.length - 1] == 'O'){
-            markersUsed.push('X')
-            console.log(markersUsed)
-            return 'O'
-        }else if(markersUsed[markersUsed.length - 1] == 'X'){
-            markersUsed.push('O')
-            console.log(markersUsed)
-            return 'X'
-        }
+  }
+  function placeMarker(gameBoard, boxId) {
+    let thisMarker = currentMarker();
+    gameBoard[boxId - 1] = thisMarker;
+    displayMarkers(gameBoard);
+    checkEnding(gameBoard);
+  }
+  let markersUsed = ["O"];
+  function currentMarker() {
+    if (markersUsed[markersUsed.length - 1] == "O") {
+      markersUsed.push("X");
+      return "O";
+    } else if (markersUsed[markersUsed.length - 1] == "X") {
+      markersUsed.push("O");
+      console.log(markersUsed);
+      return "X";
     }
-    return { checkEnding, placeMarker }
-})()
+  }
+  return { placeMarker };
+})();
 
-for(let i = 1; i <= 9; i++){
-    let divI = document.getElementById(i);
-    divI.addEventListener("click",() => displayController.placeMarker(
-        gameBoard,
-        i
-    ))
+for (let i = 1; i <= 9; i++) {
+  let divI = document.getElementById(i);
+  divI.addEventListener("click", () =>
+    displayController.placeMarker(gameBoard, i)
+  );
 }
 
-console.log(gameBoard[0])
-console.log(playerOne["currentMarkers"], playerTwo)
+console.log(gameBoard[0]);
+console.log(playerOne["currentMarkers"], playerTwo);
